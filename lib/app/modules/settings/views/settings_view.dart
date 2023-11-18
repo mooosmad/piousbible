9831a00ff2500122456913295291e6815d5a6983
lamar02 <lioneladjei02@gmail.com>
2023-11-18 23:02:20 +0000

    front_reglages

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -10,13 +10,137 @@ class SettingsView extends GetView<SettingsController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SettingsView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'SettingsView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundImage: AssetImage("assets/img_profile.png"),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      child: Text(
+                        "Rachelle Nguessan",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Profil",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Image.asset(
+                                "assets/arrow-right.png",
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Notification",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Switch(
+                                  value: false,
+                                  onChanged: (value) {
+                                    print(value);
+                                  })
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Langue",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Image.asset(
+                                "assets/arrow-right.png",
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Dark mode",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Switch(
+                                  value: false,
+                                  onChanged: (value) {
+                                    print(value);
+                                  })
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+              //  Expanded(
+              //  child: ListView.builder(
+              //  itemCount: 3,
+              // itemBuilder: (context, index) {
+              //  return ListTile(
+              //   title: Text(
+              //    index == 0
+              //       ? "Profile"
+              //      : index == 1
+              //         ? "Notifications"
+              //         : "Langue",
+              // style: TextStyle(fontSize: 18),
+              //),
+              //);
+              // },
+              //),
+              // ),
+            ],
+          ),
         ),
       ),
     );
