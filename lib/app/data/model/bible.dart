1e5fd82d9572c3a43c69a9df05c2912f8313e64b
mooosmad <mooosmad@gmail.com>
2023-12-12 23:38:55 +0000

    feat: add bible model

---
+++ lib/app/data/model/bible.dart
@@ -0,0 +1,80 @@
+class Bible {
+  final String book;
+  final String chapter;
+  final String verse;
+  final String text;
+
+  Bible(
+      {required this.book,
+      required this.chapter,
+      required this.verse,
+      required this.text});
+}
+
+List<Bible> bibleList = [
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "1",
+      text: "In the beginning God created the earth and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "2",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "3",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "4",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "5",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "6",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "7",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "8",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "9",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "10",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+  Bible(
+      book: "Genesis",
+      chapter: "1",
+      verse: "11",
+      text:
+          "and called it, and called it, and called it, and called it, and called it,"),
+];
