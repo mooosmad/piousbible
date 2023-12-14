3530c1a3a094ae340e15ec148c8262bfcd6063a7
mooosmad <mooosmad@gmail.com>
2023-12-14 23:27:38 +0000

    fix: data api consume

--- lib/app/data/model/bible.dart
+++ lib/app/data/model/bible.dart
@@ -25,16 +25,143 @@ List<Bible> bibleList = [
   ),
   Bible(
     book: "Exode",
-    chapter: "20",
+    chapter: "1",
     verse: "1",
     text: "Alors Dieu prononça toutes ces paroles, en disant :",
     testament: "Ancien",
   ),
+
   Bible(
-    book: "Psaumes",
-    chapter: "23",
+    book: "Leviticus",
+    chapter: "26",
     verse: "1",
-    text: "L'Éternel est mon berger: je ne manquerai de rien.",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+  Bible(
+    book: "Nombres",
+    chapter: "11",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+  Bible(
+    book: "Deuteronomie",
+    chapter: "30",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Jude",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Ruth",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "1 Samue",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "2 Samue",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "1 Chroniques",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "2 Chroniques",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Esaiens",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Péri",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Psalms",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Proverbes",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Ecclesiastes",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Cantiques",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Séantiques",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Ancien",
+  ),
+
+  Bible(
+    book: "Isaié",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
     testament: "Ancien",
   ),
 
@@ -55,12 +182,69 @@ List<Bible> bibleList = [
         "Car Dieu a tant aimé le monde qu'il a donné son Fils unique, afin que quiconque croit en lui ne périsse point, mais qu'il ait la vie éternelle.",
     testament: "Nouveau",
   ),
+
   Bible(
     book: "Actes",
-    chapter: "2",
+    chapter: "1",
     verse: "1",
     text:
-        "Le jour de la Pentecôte, ils étaient tous ensemble dans le même lieu.",
+        "Dieu est mon ange et mon peuple. Jusqu'à ce jour, tout le monde est devant moi.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "Romains",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "1 Corantiens",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "2 Corantiens",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "Galatas",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "Efés",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "Philipiens",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
+    testament: "Nouveau",
+  ),
+
+  Bible(
+    book: "Colossiens",
+    chapter: "1",
+    verse: "1",
+    text: "Dieu est mon ange et mon peuple.",
     testament: "Nouveau",
   ),
 ];
