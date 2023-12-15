7dfcdccf717730796ef163a04093738fba92e83b
mooosmad <mooosmad@gmail.com>
2023-12-15 01:33:26 +0000

    fix:chpaterlistview

---
+++ lib/app/modules/bible/views/detailschapter_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pioubible/app/components/testament_container.dart';
+import 'package:pioubible/app/data/model/bible.dart';
+import 'package:pioubible/app/modules/bible/views/detailsversebychapter_view.dart';
+
+class ChapterListPage extends StatelessWidget {
+  final List<Bible> testamentList; // Ajoutez la déclaration du paramètre
+
+  const ChapterListPage({Key? key, required this.testamentList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Liste des Chapitres'),
+      ),
+      body: ListView.builder(
+        itemCount: testamentList.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+              onTap: () {
+                Get.to(() => DetailsversebychapterView());
+              },
+              child: ListTile(
+                title: Text(testamentList[index].chapter),
+              ));
+        },
+      ),
+    );
+  }
+}
